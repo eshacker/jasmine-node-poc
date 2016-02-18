0d68e5f9c77828ce6b3935baa7c6756316677e4e
Ecmascript Hacker <Ecmascript.Hacker@gmail.com>
2016-02-18 10:16:51 +0545

    Failing tests.

---
+++ spec/index.spec.js
@@ -0,0 +1,20 @@
+var app = require('../src/index.js');
+
+describe('Testing basic mathematics on jasmine node.', function(){
+  var x, y;
+  beforeEach(function(){
+    x = 10;
+    y = 5;
+  });
+  afterEach(function(){
+    x = 10;
+    y = 5;
+  });
+  it('sum of 10 and 5 is 15', function(){
+    expect(app.sum(x, y)).toBe(5);
+  });
+  it('difference of 10 and 5 is 5', function(){
+    expect(app.diff(x, y)).toBe(15);
+  });
+});
+
